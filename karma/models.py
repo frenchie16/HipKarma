0e38d907a8713529403267ff6877bfea71be56ea
John French <jdaviesf7@gmail.com>
2014-08-23 09:30:11 -0700

    Fixed some issues in manage and improved models

--- karma/models.py
+++ karma/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+A Karmic Entity is a thing that can have karma. It could be a user, or just some string.
+"""
+class KarmicEntity(models.Model):
+
+    """
+    If this represents a user, the ID of the user (NOT their email nor mention name,
+    as these can change), or else the string (for when people give inanimate objects
+    karma)
+    """    
+    id = models.CharField('id', max_length=50, primary_key=True, db_index=True)
+    
+    """
+    True if id is the ID of a user, false otherwise (if 'id' is just some string).
+    """
+    is_user = models.BooleanField('is user')
+    
+    
+
 class Karma(models.Model):
 
     GOOD = 'G'
@@ -12,21 +32,14 @@ class Karma(models.Model):
     )
 
     """
-    If the recipient is a user, the ID of the user (NOT their email nor mention name,
-    as these can change), or else the string (for when people give inanimate objects
-    karma)
+    The recipient of this karma
     """
-    recipient = models.CharField('recipient', max_length=50, db_index=True)
+    recipient = models.ForeignKey('KarmicEntity', name='recipient', related_name='received', db_index=True)
 
     """
-    True if 'who' is the ID of a user, false otherwise (if 'who' is just some string).
+    The sender of this karma
     """
-    is_user = models.BooleanField('is user')
-
-    """
-    The ID of the user (not email nor mention name) who gave the karma.
-    """
-    sender = models.CharField('sender', max_length=50)
+    sender = models.ForeignKey('KarmicEntity', name='sender', related_name='sent', db_index=True)
 
     """
     The karma value, one of good or bad (could add others like neutral)

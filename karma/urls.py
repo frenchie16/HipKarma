b48fa972a42488225de0e4d70aca05abd31146e9
John French <jdaviesf7@gmail.com>
2014-09-06 12:01:36 -0700

    Make trailing / in urls optional (without the redirect provided by django)

--- karma/urls.py
+++ karma/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = patterns('karma',
                        url(r'^$', views.index, name='index'),
-                       url(r'^capabilities/$', views.capabilities, name='capabilities'),
-                       url(r'^install/$', views.install, name='install'),
+                       url(r'^capabilities/?$', views.capabilities, name='capabilities'),
+                       url(r'^install/?$', views.install, name='install'),
                        url(r'^install/(?P<client_id>\S*$)', views.uninstall, name='uninstall'),
-                       url(r'^hooks/give/$', views.give_hook, name='hooks.give'),
-                       url(r'^hooks/show/$', views.show_hook, name='hooks.show'),
-                       url(r'^hooks/help/$', views.help_hook, name='hooks.help'))
+                       url(r'^hooks/give/?$', views.give_hook, name='hooks.give'),
+                       url(r'^hooks/show/?$', views.show_hook, name='hooks.show'),
+                       url(r'^hooks/help/?$', views.help_hook, name='hooks.help'))
